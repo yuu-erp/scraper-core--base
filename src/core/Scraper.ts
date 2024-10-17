aba90b95d80194955193b05c16e1f05a99d6afdc
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2024-10-17 12:48:03 +0700

    [UPDATE] PuppeteerScraper

--- src/core/Scraper.ts
+++ src/core/Scraper.ts
@@ -3,6 +3,7 @@ import Proxy from "./Proxy";
 import logger from "../logger";
 import { RequireAtLeastOne } from "~/types/utils";
 import { SourceAnime, SourceManga } from "~/types/data";
+import { PuppeteerScraper } from "./PuppeteerScraper";
 
 export const DEFAULT_CONFIG: AxiosRequestConfig = {};
 
@@ -21,6 +22,8 @@ export class Scraper {
   disableMonitor: boolean;
   proxy: Proxy;
   locales: string[];
+  puppeteerScraper: PuppeteerScraper; // Puppeteer scraper instance
+
   constructor(
     id: string,
     name: string,
@@ -50,6 +53,7 @@ export class Scraper {
     });
     this.blacklistTitles = ["live action"];
     this.locales = [];
+    this.puppeteerScraper = new PuppeteerScraper(); // Initialize PuppeteerScraper
   }
   async init() {
     console.log("Scraper");
